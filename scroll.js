eda4ed897a8d7fc189ef5eab7d73c7b81e38156f
shaybc <shay.public@gmail.com>
2022-05-30 01:04:14 +0300

    a

--- scroll.js
+++ scroll.js
@@ -32,6 +32,3 @@ const storeScroll = () => {
 
 // Listen for new scroll events, here we debounce our `storeScroll` function
 document.addEventListener('scroll', debounce(storeScroll), { passive: true });
-
-// Update scroll position for first time
-storeScroll();
